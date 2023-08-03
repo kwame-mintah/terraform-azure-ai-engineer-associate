91d7f41db3d66b20a9615beeacc90e5f96f3247c
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2023-08-03 18:51:59 +0100

    feat(cognitivie_services): configure an alert rule on cognitivie service
    new activity log alert rule created for logging when a list keys action has performed. this alert will be applied to the cognitive service created.

--- modules/cognitive_services/main.tf
+++ modules/cognitive_services/main.tf
@@ -19,3 +19,21 @@ resource "azurerm_cognitive_account" "cognitive_services_account" {
     local.common_tags
   )
 }
+
+
+resource "azurerm_monitor_activity_log_alert" "cognitive_services_alert" {
+  name                = "${var.name}-activitylogalert"
+  resource_group_name = var.resource_group_name
+  scopes              = [azurerm_cognitive_account.cognitive_services_account.id]
+  description         = "This alert will monitor Cognitive Services API account list keys."
+
+  criteria {
+    resource_id    = azurerm_cognitive_account.cognitive_services_account.id
+    operation_name = "Microsoft.CognitiveServices/accounts/listKeys/action" # https://learn.microsoft.com/en-us/rest/api/cognitiveservices/accountmanagement/operations/list?tabs=HTTP#code-try-0
+    category       = "Administrative"
+  }
+
+  tags = merge(
+    local.common_tags
+  )
+}
